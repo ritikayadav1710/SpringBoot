3bd443ecfd8e74126fe557fbe335f985889e972c
Ritika <ritika.yadav@vedalytics.com>
2025-02-17 15:21:26 +0530

    Springboot learning

---
+++ DemoSpring/src/main/java/org/example/Dev.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Dev {
+    public Dev(){
+        System.out.println("Dev constructor");
+    }
+
+    public void build()
+    {
+
+        System.out.println("working on awesome project");
+
+    }
+}
---
+++ DemoSpring/src/main/java/org/example/Laptop.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop constructor");
+    }
+    public void compile(){
+        System.out.println("compiling");
+    }
+}
